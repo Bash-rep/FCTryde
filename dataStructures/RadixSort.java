3bedab86f503a2ef65433fe574ab59d93630b42f
bash <bash@bash-MS-7971>
2019-11-11 19:54:19 +0000

    ok?

--- dataStructures/RadixSort.java
+++ dataStructures/RadixSort.java
@@ -3,50 +3,6 @@ package dataStructures;
 
 public class RadixSort<V> {
 	
-	public class AOCL<V> {
-		
-		DoublyLinkedList<Entry<Integer,V>>[] elements;
-		int size;
-		
-		@SuppressWarnings("unchecked")
-		public AOCL(int size) {
-			elements = new DoublyLinkedList[size];
-			this.size = size;
-			initialize(size);
-		}
-		
-		public void initialize(int size){
-			this.size = size;
-			
-			for(int i = 0 ; i < size ; i++) {
-				elements[i] = new DoublyLinkedList<Entry<Integer,V>>();
-			}
-		}
-		public void insert(int index,Entry<Integer, V> element) {
-			elements[index].addLast(element);
-		}
-		
-		public DListNode<Entry<Integer, V>> concatAll() {
-			DListNode<Entry<Integer, V>> tail = new DListNode<Entry<Integer, V>>(null), head = null, finalHead = null;
-
-			for (int i = 0; i < size; i++) {
-				if (elements[i].head != null) {
-					if (finalHead == null) {
-						finalHead = elements[i].head;
-					}
-					head = elements[i].head;
-					tail.next = head;
-					head.previous = tail;
-					tail = elements[i].tail;
-				}
-			}
-
-			initialize(size);
-			return finalHead;
-		}
-	}
-
-	
 	
 	public  List<Entry<Integer, V>> sort(DoublyLinkedList<Entry<Integer, V>> sortables, int maxSize) {
 		
